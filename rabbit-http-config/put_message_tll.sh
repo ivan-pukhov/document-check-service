4e3a856bdb2caa471e06d04fb2e2f494b5c08970
gidevan <vsanyc@gmail.com>
2025-04-16 23:07:49 +0300

    Moved ttl config in policies

--- rabbit-http-config/put_message_tll.sh
+++ rabbit-http-config/put_message_tll.sh
@@ -2,4 +2,4 @@
 
 echo Put policy message-ttl
 
-curl -u document-check:document-check -XPUT -H "content-type:application/json" --url http://localhost:15672/api/policies/document-check-host/document-message-ttl -d@ttl-policy-config.json -i
+curl -u document-check:document-check -XPUT -H "content-type:application/json" --url http://localhost:15672/api/policies/document-check-host/document-check-queue.dlq.message-ttl -d@ttl-policy-config.json -i
