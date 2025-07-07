45371a2a4917f538bc0a6fc22e70aac409a30fd9
ipukhov <ipukhov@cft.ru>
2025-07-07 17:23:58 +0300

    Readme is updated

--- government-service-adapter/src/main/java/org/example/government/service/adapter/model/DocumentStatus.java
+++ government-service-adapter/src/main/java/org/example/government/service/adapter/model/DocumentStatus.java
@@ -1,5 +1,28 @@
 package org.example.government.service.adapter.model;
 
 public enum DocumentStatus {
-    VALID, INVALID, CHECK_IN_PROCESS, CHECK_ERROR, CHECK_COMPLETED_WITH_ERROR
+    /**
+     * Document is valid
+     */
+    VALID,
+
+    /**
+     * Document is invalid
+     */
+    INVALID,
+
+    /**
+     * Check document is in process.
+     */
+    CHECK_IN_PROCESS,
+
+    /**
+     * Document Checking is completed with an error. Document will be checked again after some time
+     */
+    CHECK_ERROR,
+
+    /**
+     * Document Checking is completed with an error. Document will not be checked again
+     */
+    CHECK_COMPLETED_WITH_ERROR
 }
